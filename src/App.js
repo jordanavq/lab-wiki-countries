464bf7ae08066a4661cb905f5e547d955667aa53
jordanavq <jordanavq@gmail.com>
2021-07-25 15:13:13 -0300

    done sem bonus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import countries from '../src/countries.json';
+import CountriesList from '../src/components/CountriesList';
+import CountryDetails from '../src/components/CountryDetails';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={countries}></CountriesList>
+          <Switch>
+            <Route
+              path="/:cca3"
+              render={(props) => (
+                <CountryDetails {...props} countries={countries} />
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
